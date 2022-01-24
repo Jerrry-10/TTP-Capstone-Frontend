0372fc5706ded4a56ca9cbc36638b9a6a4fdb4e1
Jerry <78663777+Jerrry-10@users.noreply.github.com>
2022-01-24 10:30:16 -0500

    navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -4,8 +4,8 @@ import { Link } from 'react-router-dom'
 export default function Navbar() {
     return (
         <div>
-            <ul id="Nav">
-                <li className = "Nav"><Link to = "/" className = "NavText">Home page</Link> </li>
+            <ul className = 'navbar' id="Nav">
+                <li className = "NavText"><Link to = "/" className = "NavText">Home page</Link> </li>
                
             </ul>
         </div>
