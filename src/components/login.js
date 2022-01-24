f99340977fbf733541b27a20ed5c9cf269469448
Jerry <78663777+Jerrry-10@users.noreply.github.com>
2022-01-24 11:57:49 -0500

    login

--- src/components/login.js
+++ src/components/login.js
@@ -2,16 +2,18 @@ import React, {useState} from 'react'
 export default function Login(){
     return(
         <div>
-        <form >
+        <form className='container'>
           <div>
             <label htmlFor="userName">User Name</label>
-            <input type="text" name="userName"   />
+            <input className="form-control" type="text" name="userName"   />
           </div>
           <div>
             <label htmlFor="password">Password</label>
-            <input type="password" name="password" />
+            <input className="form-control" type="password" name="password" />
           </div>
-          <button>Log In</button>
+          <div class="d-grid gap-2">
+            <button className="btn btn-block btn-primary" type="button">Button</button>
+          </div>
         </form>
         </div>
     )
