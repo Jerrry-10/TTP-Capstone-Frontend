3e46892a99e3f4e3bcebe679f299b98ea8a61eea
Jerry <78663777+Jerrry-10@users.noreply.github.com>
2022-01-24 14:25:17 -0500

    Adds Register/login

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,22 +1,29 @@
 import React, {useState} from 'react'
-import { Link } from 'react-router-dom'
 export default function Login(){
     return(
         <div>
         <form className='container'>
           <div>
-            <label htmlFor="userName">User Name</label>
-            <input className="form-control" type="text" name="userName"   />
+            <label htmlFor="FirstName">First Name</label>
+            <input className="form-control" type="text" name="FirstName"   />
+          </div>
+          <div>
+            <label htmlFor="Lastname">Last name</label>
+            <input className="form-control" type="text" name="Lastname" />
+          </div>
+          <div>
+            <label htmlFor="email">email</label>
+            <input className="form-control" type="email" name="email" />
           </div>
           <div>
             <label htmlFor="password">Password</label>
             <input className="form-control" type="password" name="password" />
           </div>
+          
           <div class="d-grid gap-2">
             <button className="btn btn-block btn-primary" type="button">Button</button>
           </div>
         </form>
-        <span> Don't have an account? <Link to = "/Register" > Register here!</Link></span>
         </div>
     )
 }
