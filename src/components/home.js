39ca039989c2d2ef38198a2ad2b7dd23257cd406
Victoria Jaczynski <victoria.jacz.14@gmail.com>
2022-01-24 15:22:30 -0500

    thinking about home page

--- src/components/home.js
+++ src/components/home.js
@@ -10,25 +10,7 @@ export default function Home() {
     return (
         <div class="container">
          <img src="" class="logo"/>
-         
-         <nav class="navbar navbar-light" style="background-color: #e3f2fd;">
-         <div class="container-fluid">
-         <a class="navbar-brand" href="#">Navbar</a>
-         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-         <span class="navbar-toggler-icon"></span>
-         </button>
-
-         <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-        <div class="navbar-nav">
-        <a class="nav-link active" aria-current="page" href="#">Home</a>
-        <a class="nav-link" href="#">Log In</a>
-        <a class="nav-link" href="#">User Profile</a>
-        <a class="nav-link" href="#">Your Information</a>
-        <a class="nav-link" href="#">Goals</a>
-      </div>
-    </div>
-  </div>
-</nav>
+   
 
          <div class="row">
             <div class="col">
@@ -42,11 +24,11 @@ export default function Home() {
 
                     
 
-        {/* <div class="row">
+        <div class="row">
             <div class="col">
                 <h1> not sure </h1>
                 <h2> not sure </h2>
-            </div> */}
+            </div> 
 
 
 
@@ -74,5 +56,5 @@ export default function Home() {
 
         </div>
       </div>
-     // </div>
+  </div>
     )}
