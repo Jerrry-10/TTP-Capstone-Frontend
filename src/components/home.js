c7c5254f1c0d622dc627912f35d139d9dcad74cc
Victoria Jaczynski <victoria.jacz.14@gmail.com>
2022-01-24 20:34:01 -0500

    working on homepage

--- src/components/home.js
+++ src/components/home.js
@@ -1,63 +1,13 @@
-import React from 'react'
-import Navbar from './navbar'
-import Login from './login'
-import UserProfile from './userprofile'
-import Information from './information'
-import Goals from './goals'
+import React from 'react';
+import Navbar from './navbar';
+import Login from './login';
 
-
-export default function Home() {
-    const redirect = useNavigate()
+function home () {
     return (
-        
-        <div class="container">
-         <img src="" class="logo"/>
-   
-
-         <div class="row">
-            <div class="col">
-                <h1> Healthy Friend </h1>
-                <h2> The project that is designed to help you with your personal goals and help you manage them!
-                   --- Log In to the Left --- </h2>
-                    </div>
-
-         <div className="d-flex justify-content-center">
-                <button type="button" class="btn btn-outline-info m-1" onClick={ () => {
-                    redirect("/login")
-                }}>Make an Account Here!</button>
-            </div>
-
-        <div class="row">
-            <div class="col">
-                <h1> not sure </h1>
-                <h2> not sure </h2>
-            </div> 
-
-
-
-            
-{/* 
-            <div class="col">
-                <div class="card card1">
-                    <h5>card here</h5>
-                  
-                </div>
-                <div class="card card2">
-                    <h5>card here</h5>
-                    
-                </div>
-                <div class="card card3">
-                    <h5>card here</h5>
-                   
-                </div>
-                <div class="card card4">
-                    <h5> </h5>
-                    
-                </div>
-            </div> */}
-
-
+        <div>
+            <h1>Hello</h1>
         </div>
-      </div>
-  </div>
-    )}
+    );
+}
+
+export default home;
