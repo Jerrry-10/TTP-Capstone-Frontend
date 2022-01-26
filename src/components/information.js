3081cfb82c82524d5ffe8eab180e78ef167f2456
Victoria Jaczynski <victoria.jacz.14@gmail.com>
2022-01-26 11:03:54 -0500

    fixed information

--- src/components/information.js
+++ src/components/information.js
@@ -4,17 +4,17 @@ const information = () => {
   return (
     <div className="Info-body">
     
-       <header className="Info-header">INFORMATION</header> 
+       <header className="Info-header">This is an information page for COVID-19</header> 
        <body>
-         <h3>Covid-19 common symptom</h3>
+         <h3>COVID-19 Common Symptoms</h3>
             <ul className="a">
               <li>Fever</li>
               <li>Cough</li>
               <li>Tired</li>
               <li>Loss of Smell or Taste</li>
             </ul>          
-         <h3>Test Site</h3>
-         <p className="column">If you have symptom of Covid-19 or exposure to covid-19, you can choose a testing site near you to get tested.</p>
+         <h3>Testing Sites:</h3>
+         <p className="column2">If you have symptoms of COVID-19 or exposure to COVID-19, you can choose a testing site near you to get tested.</p>
          <form action='https://coronavirus.health.ny.gov/find-test-site-near-you'>
            <button className="column1 button" type='submit'>Find Test Site</button>
          </form>
@@ -22,8 +22,8 @@ const information = () => {
          <br />
          <br />
          <br />
-         <h3>Vaccine Site</h3>
-         <p className="column">To project you and people around you from Covid-19, Please get vacinated if you could.</p>
+         <h3>Vaccine Sites:</h3>
+         <p className="column2">To protect you and people around you from COVID-19, Please consider to get vacinated if possible.</p>
          <form action='https://www.vaccines.gov/search/'>
          <button className="column1 button" type='submit'>Find Vaccine Site</button>
          </form>
